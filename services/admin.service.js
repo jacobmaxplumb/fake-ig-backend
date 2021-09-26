43621b3dc18ddb61c06e29a44a81649376ab907d
jacobplumblambda <jacob.plumb@lambdaschool.com>
2021-09-26 01:34:10 -0400

    lots of changes

--- services/admin.service.js
+++ services/admin.service.js
@@ -2,13 +2,7 @@ const admin = require('firebase-admin');
 var serviceAccount = require('../fake-ig-demo-firebase-adminsdk-df558-213185573c.json');
 
 admin.initializeApp({
-    apiKey: "AIzaSyAMNmOavSsAwKoNDTEVBJTVzCqrhvG1Ld4",
-    authDomain: "fake-ig-demo.firebaseapp.com",
-    projectId: "fake-ig-demo",
-    storageBucket: "fake-ig-demo.appspot.com",
-    messagingSenderId: "736644692439",
-    appId: "1:736644692439:web:5ce6778a59b5d531e7f392",
-    measurementId: "G-RNXWZFEG7B"
+    credential: admin.credential.cert(serviceAccount)
 });
 
 module.exports = admin;
